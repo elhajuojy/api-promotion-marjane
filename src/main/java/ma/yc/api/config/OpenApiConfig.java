0e1723d3fd141c17ad8c158a4b60435ccee5ffa5
e.elhjoujy <e.elhjoujy@labelvie.ma>
2023-12-02 22:49:53 +0100

    [FIX ] [petstore URL not disabled in SpringDoc OpenAPi ] [ Ability to disable swagger-ui default petstore url #714]

--- src/main/java/ma/yc/api/config/OpenApiConfig.java
+++ src/main/java/ma/yc/api/config/OpenApiConfig.java
@@ -3,18 +3,30 @@ package ma.yc.api.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.servers.Server;
 
 import java.util.Arrays;
+import java.util.logging.Logger;
 
 @Configuration
+@Slf4j
+
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI customOpenAPI(@Value("${contact-email}") String email, @Value("${local-server.url}") String localServerUrl, @Value("${prod-server.url}") String prodServerUrl, @Value("${project.version}") String appVersion, @Value("${project.title}") String title, @Value("${project.description}") String description) {
+    public OpenAPI customOpenAPI(@Value("${contact-email}") String email,
+                                 @Value("${local-server.url}") String localServerUrl,
+                                 @Value("${prod-server.url}") String prodServerUrl,
+                                 @Value("${project.version}") String appVersion, @Value("${project.title}") String title,
+                                 @Value("${project.description}") String description
+    ) {
+        log.info("email: {}", email);
+        log.info("localServerUrl: {}", localServerUrl);
         return new OpenAPI().info(new Info()
                 .title(title)
                 .version(appVersion)
